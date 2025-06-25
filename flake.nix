41e2f2529ee02dd155389eb1d87a1f4130564b32
glanch <49610590+glanch@users.noreply.github.com>
2025-06-25 20:23:42 +0200

    Add NixOS module for basic usecase

--- flake.nix
+++ flake.nix
@@ -7,6 +7,89 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    {
+      nixosModules.default = { lib, config, pkgs, ... }:
+        with lib;
+        let
+          cfg = config.services.multicast-relay;
+        in
+        {
+          options.services.multicast-relay = {
+            enable = mkEnableOption "Enable the Python multicast-relay";
+
+            interfaces = mkOption {
+              type = types.listOf types.str;
+              description = "List of interfaces to relay between (min 2)";
+            };
+
+            relays = mkOption {
+              type = types.listOf types.str;
+              default = [ "239.255.255.250:1900" ];
+              description = "List of multicast or broadcast IP:PORT pairs to relay.";
+            };
+
+            noTransmitInterfaces = mkOption {
+              type = types.listOf types.str;
+              default = [ ];
+              description = "Interfaces to receive from but not transmit to.";
+            };
+
+            masqueradeInterfaces = mkOption {
+              type = types.listOf types.str;
+              default = [ ];
+              description = "Interfaces from which to masquerade packets (rewrite source IP).";
+            };
+
+            extraArgs = mkOption {
+              type = types.listOf types.str;
+              default = [ ];
+              description = "Additional command line arguments for multicast-relay.";
+            };
+
+            logFile = mkOption {
+              type = types.str;
+              default = "/var/log/multicast-relay.log";
+              description = "Log file path for multicast-relay.";
+            };
+
+            package = mkOption {
+              type = types.package;
+              default = pkgs.multicast-relay;
+              description = "Package to use for the multicast-relay service.";
+            };
+          };
+
+          config = mkIf cfg.enable {
+            systemd.services.multicast-relay = {
+              description = "Python-based Multicast Relay";
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = concatStringsSep " " ([
+                  "${cfg.package}/bin/multicast-relay"
+                  "--foreground"
+                  "--verbose"
+                  "--logfile ${cfg.logFile}"
+                ]
+                ++ (map (iface: "--interfaces ${iface}") cfg.interfaces)
+                ++ (map (relay: "--relay ${relay}") cfg.relays)
+                ++ (map (noTx: "--noTransmitInterfaces ${noTx}") cfg.noTransmitInterfaces)
+                ++ (map (masq: "--masquerade ${masq}") cfg.masqueradeInterfaces)
+                ++ cfg.extraArgs);
+
+                Restart = "always";
+                RestartSec = 5;
+                StandardOutput = "journal";
+                StandardError = "journal";
+              };
+            };
+          };
+
+
+        };
+    } //
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
